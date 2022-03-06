a425f4af04cf05faf58505e8038ffee1ef7d1e8b
Ofek Shmuely <meshuba.global@gmail.com>
2022-03-06 02:14:13 +0200

    1st commit

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+A very simply way to check if a number
+ is a decimal or integer is to see if 
+ there is a remainder left when you
+ divide by 1.
+*/
+
+function isInt(num) {
+  return num % 1 === 0;
+}
+
+console.log(isInt(4)); // true
+console.log(isInt(12.2)); // false
+console.log(isInt(0.3)); // false
